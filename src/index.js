b010f229ecb9bf013285fface0b3c440bdf8cf89
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2022-08-26 04:08:26 +0300

    populate the tasks to list

--- src/index.js
+++ src/index.js
@@ -1,14 +1,47 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const todos = [
+  {
+    description: 'Finish all the projects',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Workout!',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Sleep!',
+    completed: false,
+    index: 2,
+  },
+];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+const todo = document.getElementById('to-do');
 
-  return element;
-}
+window.addEventListener('load', () => {
+  for (let i = 0; i < todos.length; i += 1) {
+    const listItem = document.createElement('li');
+    listItem.classList.add('list-item');
 
-document.body.appendChild(component());
+    const inp = document.createElement('input');
+    inp.setAttribute('type', 'checkbox');
+    inp.classList.add('check');
+    inp.id = 'check';
+
+    const lbl = document.createElement('label');
+    lbl.textContent = `${todos[i].description}`;
+    lbl.classList.add('description');
+    lbl.setAttribute('for', 'check');
+
+    const update = document.createElement('button');
+    update.classList.add('update-btn');
+    update.innerHTML = '<i class="fa fa-edit"></i>';
+
+    listItem.appendChild(inp);
+    listItem.appendChild(lbl);
+    listItem.appendChild(update);
+    todo.appendChild(listItem);
+  }
+});
